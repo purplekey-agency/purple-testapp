21046f256de44000498d07b588c980850f576519
Amar Bukva <amar@purplekey.eu>
2026-04-24 15:56:02 +0200

    commit

---
+++ app/src/main/java/com/purple/testapp/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.purple.testapp
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
